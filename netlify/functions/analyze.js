e7baff9610cd4a63c520799b7efa8e596994c429
Hall <torhall@deloitte.com>
2025-09-23 12:19:10 -0500

    ADD: Minimal working function (step by step creation)

---
+++ netlify/functions/analyze.js
@@ -0,0 +1,5 @@
+import Papa from 'papaparse'
+import dayjs from 'dayjs'
+
+  return { statusCode: 200, headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify({ test: 'working', kpis: { engineering_total: 6, network_coverage_pct_est: 50, missing_on_network_est: 3, orphans_on_network: 1, outdated_high_assets_est: 2 }, rows: [], reportHtml: '<html><body><h1>Test</h1></body></html>' }) }
+}
